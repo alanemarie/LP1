9aa9f01adb2f4675a6a5579012ca4dcd54159621
alanemarie <alane93@gmail.com>
2023-08-21 11:18:58 -0300

    arquivo teste

---
+++ funcao_soma.py
@@ -0,0 +1,12 @@
+def soma(valorA, valorB):
+    soma = valorA + valorB
+    return soma 
+
+def media(valorA, valorB):
+    media = soma(valorA, valorB)
+    return media
+
+a = eval(input())
+b = eval(input())
+resultado = media(a,b)
+print(resultado)
